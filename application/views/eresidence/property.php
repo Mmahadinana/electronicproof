a289577c735724a10bfd4f9fe39aeb4b59c5fbb0
palesamotseare@gmail.com <palesamotseare@gmail.com>
2017-11-09 13:06:21 +0000

    eresidence update

---
+++ application/views/eresidence/property.php
@@ -0,0 +1,16 @@
+ <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+
+
+  <div class="starter-template">
+      <?php
+ $this->load->view('eresidence/OwnersProperty');
+
+
+ //$this->load->view($pageToLoad);
+
+ 
+?>
+</div>
