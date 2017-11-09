df17e5926ef390464ed7c1bfaf3075b72d174f35
Mmahadinana Rosy Marumo <mmahadinanarosy@gmail.com>
2017-11-09 12:16:53 +0000

    rosylastupdate

--- application/views/Contact/contact.php
+++ application/views/Contact/contact.php
@@ -1,57 +1,65 @@
   <?php
   defined('BASEPATH') OR exit('No direct script access allowed');
   ?>
-  <div class="container">
-    <section class="contact-section">
-      <!--div class="contact"-->
+ 
 
 
-        <h3>Contact Us</h3>
+    <div class="container">
+     
+      <section class="contact-section">
+        <div class="contact">
+         <div class="form-area">
 
-        <div class="row">
-          <div class="col-md-8 col-md-offset-2">
-           <?php 
-           $options = array("class"=> "form-group","method"=>"POST");
-           echo form_open("publiczone/contact",$options);
+          <h1>Contact Us</h1>
 
-           if(isset($statusInsert)){
-            echo alertMsg($statusInsert,'Message Sent','Message  Not Sent');
-          }
+          <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+             <?php 
+             $options = array("class"=> "form-group","method"=>"POST");
+             echo form_open("publiczone/contact",$options);
 
-          ?>
+             if(isset($statusInsert)){
+              echo alertMsg($statusInsert,'Message Sent','Message  Not Sent');
+            }
 
-          <div class="form-group">
-            <?php echo form_error('name') ? alertMsg(false,'',form_error('name')):'';?>
-            <label for="name">Name</label>
-            <input type="text" class="form-control" id="name"  name="name" placeholder="Palesa Motseare">
-          </div>
-          <div class="form-group">
-            <?php echo form_error('phone') ? alertMsg(false,'',form_error('phone')):'';?>
-            <label for="phone">Phone Number</label>
-            <input type="text" class="form-control" id="phone"  name="phone" placeholder="0735517010">
-          </div>
-          <div class="form-group">
-            <?php echo form_error('email') ? alertMsg(false,'',form_error('email')):'';?>
-            <label for="email">Email</label>
-            <input type="email" class="form-control" id="email"  name="email" placeholder="palesa.motseare@example.com">
-          </div>
-          <div class="form-group ">
-            <?php echo form_error('message') ? alertMsg(false,'',form_error('message')):'';?>
-            <label for="message">Your Message</label>
-            <textarea  class="form-control" id="message" name="message" maxlength="500" placeholder="Description"></textarea>
-            <span class="help-block"><p id="characterLeft" class="help-block ">You have reached the limit</p></span>
-          </div>
-          <div class="form-group">
+            ?>
+
+            <div class="form-group">
+              <?php echo form_error('name') ? alertMsg(false,'',form_error('name')):'';?>
+              <label for="name">Name</label>
+              <input type="text" class="form-control" id="name"  name="name" placeholder="Palesa Motseare">
+            </div>
+            <div class="form-group">
+              <?php echo form_error('phone') ? alertMsg(false,'',form_error('phone')):'';?>
+              <label for="phone">Phone Number</label>
+              <input type="text" class="form-control" id="phone"  name="phone" placeholder="0735517010">
+            </div>
+            <div class="form-group">
+              <?php echo form_error('email') ? alertMsg(false,'',form_error('email')):'';?>
+              <label for="email">Email</label>
+              <input type="email" class="form-control" id="email"  name="email" placeholder="palesa.motseare@example.com">
+            </div>
+            <div class="form-group ">
+              <?php echo form_error('message') ? alertMsg(false,'',form_error('message')):'';?>
+              <label for="message">Your Message</label>
+              <textarea  class="form-control" id="message" name="message" maxlength="500" placeholder="Description"></textarea>
+              <span class="help-block"><p id="characterLeft" class="help-block ">You have reached the limit</p></span>
+            </div>
+            <div class="form-group">
 
 
-          </div>
-          <button type="submit" class="btn btn-default">Send Message</button>
+            </div>
+            <button type="submit" class="btn btn-default">Send Message</button>
 
-        </form>
+          </form>
+        </div>
       </div>
     </div>
+  </div>
   </section>
+
 </div>
+
 
 <script type="text/javascript">
 	$(document).ready(function(){ 
---
+++ application/views/eresidence/OwnersProperty.php
@@ -0,0 +1,124 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<h1 class="whtColor"> List Of Owners Property </h1>
+<div class="container form-area">
+
+ 
+
+  <?php
+
+  $action="residents/property/";
+
+  echo form_open($action,array('class'=>'form-horizontal','enctype'=>'multipart/form-data'));?>
+  
+  <div class="row">
+    <div class="col-md-10">
+      <table class="table text-left">
+
+        <tbody>
+          <tr>
+            <td>Owner's Full Name</td>
+            <td>Refiloe Sibyl</td>
+          </tr>
+
+          <tr>
+            <td rowspan="9">1.Residential Address</td>               
+
+          </tr>
+
+          <tr>
+            <td>1814 Phahameng Section</td>
+
+          </tr> 
+          <tr>
+            <td>Letshuma Street</td>
+
+          </tr>
+          <tr>
+            <td>Mokwallo</td>
+
+          </tr>
+          <tr>
+            <td>VredeFort</td>
+
+          </tr>
+          <tr>
+            <td>9595</td>
+
+          </tr>
+          <tr>
+            <td>Ngwathe</td>            
+          </tr>
+
+          <tr>
+            <td>Fezile Dabi</td>            
+          </tr>
+
+          <tr>
+            <td>Free State</td>            
+          </tr>
+
+
+
+
+
+
+          <tr>
+            <td rowspan="9">2.Residential Address </td>               
+
+          </tr>
+
+          <tr>
+            <td>1814 Phahameng Section</td>
+
+          </tr> 
+          <tr>
+            <td>Letshuma Street</td>
+
+          </tr>
+          <tr>
+            <td>Mokwallo</td>
+
+          </tr>
+          <tr>
+            <td>VredeFort</td>
+
+          </tr>
+          <tr>
+            <td>9595</td>
+
+          </tr>
+          <tr>
+            <td>Ngwathe</td>            
+          </tr>
+
+          <tr>
+            <td>Fezile Dabi</td>            
+          </tr>
+
+          <tr>
+            <td>Free State</td>            
+          </tr>
+
+
+
+
+
+
+        </tbody>
+      </table>
+
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+</div>
