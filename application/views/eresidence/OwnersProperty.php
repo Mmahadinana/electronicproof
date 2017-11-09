28d24c1c22baf8965657d35595ce701a877ae59a
palesamotseare@gmail.com <palesamotseare@gmail.com>
2017-11-09 17:38:47 +0000

    update

--- application/views/eresidence/OwnersProperty.php
+++ application/views/eresidence/OwnersProperty.php
@@ -7,100 +7,100 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  <h2 class="whtColor"> List Of Owners Property </h2>
 
 
-  <?php
+ <?php
 
-  $action="residents/property/";
+ $action="residents/property/";
 
-  echo form_open($action,array('class'=>'form-horizontal','enctype'=>'multipart/form-data'));?>
-  
-  <div class="row">
-    <div class="col-md-10">
-      <table class="table text-left">
+ echo form_open($action,array('class'=>'form-horizontal','enctype'=>'multipart/form-data'));?>
+ 
+ <div class="row">
+  <div class="col-md-10">
+    <table class="table text-left">
 
-        <tbody>
-          <tr>
-            <td>Owner's Full Name</td>
-            <td>Refiloe Sibyl</td>
-          </tr>
+      <tbody>
+        <tr>
+          <td>Owner's Full Name</td>
+          <td>Refiloe Sibyl</td>
+        </tr>
 
-          <tr>
-            <td rowspan="9">1.Residential Address</td>               
+        <tr>
+          <td rowspan="9">1.Residential Address</td>               
 
-          </tr>
+        </tr>
 
-          <tr>
-            <td>1814 Phahameng Section</td>
+        <tr>
+          <td>1814 Phahameng Section</td>
 
-          </tr> 
-          <tr>
-            <td>Letshuma Street</td>
+        </tr> 
+        <tr>
+          <td>Letshuma Street</td>
 
-          </tr>
-          <tr>
-            <td>Mokwallo</td>
+        </tr>
+        <tr>
+          <td>Mokwallo</td>
 
-          </tr>
-          <tr>
-            <td>VredeFort</td>
+        </tr>
+        <tr>
+          <td>VredeFort</td>
 
-          </tr>
-          <tr>
-            <td>9595</td>
+        </tr>
+        <tr>
+          <td>9595</td>
 
-          </tr>
-          <tr>
-            <td>Ngwathe</td>            
-          </tr>
+        </tr>
+        <tr>
+          <td>Ngwathe</td>            
+        </tr>
 
-          <tr>
-            <td>Fezile Dabi</td>            
-          </tr>
+        <tr>
+          <td>Fezile Dabi</td>            
+        </tr>
 
-          <tr>
-            <td>Free State</td>            
-          </tr>
+        <tr>
+          <td>Free State</td>            
+        </tr>
 
 
-          <tr>
-            <td rowspan="9">2.Residential Address </td>               
+        <tr>
+          <td rowspan="9">2.Residential Address </td>               
 
-          </tr>
+        </tr>
 
-          <tr>
-            <td>1814 Phahameng Section</td>
+        <tr>
+          <td>1814 Phahameng Section</td>
 
-          </tr> 
-          <tr>
-            <td>Letshuma Street</td>
+        </tr> 
+        <tr>
+          <td>Letshuma Street</td>
 
-          </tr>
-          <tr>
-            <td>Mokwallo</td>
+        </tr>
+        <tr>
+          <td>Mokwallo</td>
 
-          </tr>
-          <tr>
-            <td>VredeFort</td>
+        </tr>
+        <tr>
+          <td>VredeFort</td>
 
-          </tr>
-          <tr>
-            <td>9595</td>
+        </tr>
+        <tr>
+          <td>9595</td>
 
-          </tr>
-          <tr>
-            <td>Ngwathe</td>            
-          </tr>
+        </tr>
+        <tr>
+          <td>Ngwathe</td>            
+        </tr>
 
-          <tr>
-            <td>Fezile Dabi</td>            
-          </tr>
+        <tr>
+          <td>Fezile Dabi</td>            
+        </tr>
 
-          <tr>
-            <td>Free State</td>            
-          </tr>
-        </tbody>
-      </table>
+        <tr>
+          <td>Free State</td>            
+        </tr>
+      </tbody>
+    </table>
 
-    </div>
   </div>
+</div>
 </div>
 </div>
--- application/views/eresidence/ownersDetails.php
+++ application/views/eresidence/ownersDetails.php
@@ -1,170 +1,175 @@
 <?php
- defined('BASEPATH') OR exit('No direct script access allowed');
- ?>
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 
-<div>
- 	<h1>Owners Information</h1>
+<div class="container form-area">
+	<div>
+		<h1>Owners Information</h1>
 
- </div>
- <div class="container form-area">
- 
-  
+	</div>
+	
 
-<div class="stepwizard col-md-offset-3">
-			<div class="stepwizard-row setup-panel">
-				<div class="stepwizard-step">
-					<a href="#step-1" type="button" class="btn btn-primary btn-circle"><i class="fa fa-lock" aria-hidden="true"></i></a>
-					<p>Step 1</p>
+	<div class="stepwizard col-md-offset-3">
+		<div class="stepwizard-row setup-panel">
+			<div class="stepwizard-step">
+				<a href="#step-1" type="button" class="btn btn-primary btn-circle"><i class="fa fa-lock" aria-hidden="true"></i></a>
+				<p>Step 1</p>
+			</div>
+			<div class="stepwizard-step">
+				<a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled"><i class="fa fa-user" aria-hidden="true"></i></a>
+				<p>Step 2</p>
+			</div>
+			<div class="stepwizard-step">
+				<a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+				<p>Step 3</p>
+			</div>
+			<div class="stepwizard-step">
+				<a href="#step-4" type="button" class="btn btn-default btn-circle" disabled="disabled"><i class="fa fa-home" aria-hidden="true"></i></a>
+				<p>Step 4</p>
+			</div>
+			
+		</div>
+	</div>
+	
+
+	<?php
+	
+	$action="residents/details/";
+
+	echo form_open($action,array('class'=>'form-horizontal','enctype'=>'multipart/form-data'));?>
+	<div class="row setup-content" id="step-1">
+		<div class="col-xs-6 col-md-offset-3">
+			<div class="col-md-12">
+				<h3>Personal Information</h3>
+				<div class="form-group">
+					<label class="control-label">Full Name</label>
+					<input maxlength="100" type="text" required="required" class="form-control" placeholder="Full Name" />
 				</div>
-				<div class="stepwizard-step">
-					<a href="#step-2" type="button" class="btn btn-default btn-circle" disabled="disabled"><i class="fa fa-user" aria-hidden="true"></i></a>
-					<p>Step 2</p>
+				<div class="form-group">
+					<label class="control-label">Identity Number</label>
+					<input maxlength="13" type="text" required="required" class="form-control" placeholder="Identity Number"  />
 				</div>
-				<div class="stepwizard-step">
-					<a href="#step-3" type="button" class="btn btn-default btn-circle" disabled="disabled"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
-					<p>Step 3</p>
+				<div class="form-group">
+					<label class="control-label">Date of Birth</label>
+					<input  type="text" required="required" class="form-control" placeholder="Date of Birth"  />
 				</div>
-				<div class="stepwizard-step">
-					<a href="#step-4" type="button" class="btn btn-default btn-circle" disabled="disabled"><i class="fa fa-home" aria-hidden="true"></i></a>
-					<p>Step 4</p>
+				<div class="form-group">
+					<label class="control-label">Email</label>
+					<input   type="email" required="required" class="form-control" placeholder="Email Address"/>
 				</div>
-				
+
+				<div class="form-group">
+					<label class="control-label">Phone number</label>
+					<input maxlength="10" type="text" required="required" class="form-control" placeholder="Phone number"  />
+				</div>
+				<label  id ="gender" class="control-label">Gender</label>
+
+				<div class="form-group">
+
+					<input name="group100" type="radio" id="radio100">
+					<label for="radio100">Male</label>
+				</div>
+
+				<div class="form-group">
+					<input name="group100" type="radio" id="radio101" checked>
+					<label for="radio101">Female</label>
+				</div>
+				<button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
 			</div>
 		</div>
- 
+	</div>
+	<div class="row setup-content" id="step-2">
+		<div class="col-xs-6 col-md-offset-3">
+			<div class="col-md-12">
+				<h3>Residential Information</h3>
+				<div class="form-group">
+					<label class="control-label">Street Address</label>
+					<input maxlength="100" type="text" required="required" class="form-control" placeholder="Street Address" />
+				</div>
+				<div class="form-group">
+					<label class="control-label">suburb</label>
+					<input maxlength="13" type="text" required="required" class="form-control" placeholder="suburb"  />
+				</div>
+				<div class="form-group">
+					<label class="control-label">City/Town</label>
+					<input  type="text" required="required" class="form-control" placeholder="City/Town"  />
+				</div>
+				<div class="form-group">
+					<label class="control-label">Zip Code</label>
+					<input maxlength="10" type="text" required="required" class="form-control" placeholder="Zip Code"  />
+				</div>
+				<div class="form-group">
+					<label class="control-label">Municipality</label>
+					<input maxlength="10" type="text" required="required" class="form-control" placeholder="Municipality"  />
+				</div>
+				<div class="form-group">
+					<label class="control-label">District</label>
+					<input maxlength="10" type="text" required="required" class="form-control" placeholder="District"  />
+				</div>
+				<div class="form-group">
+					<label class="control-label">Province</label>
+					<input maxlength="10" type="text" required="required" class="form-control" placeholder="Province"  />
+				</div>
 
-<?php
- 
- $action="residents/details/";
-
- echo form_open($action,array('class'=>'form-horizontal','enctype'=>'multipart/form-data'));?>
- <div class="row setup-content" id="step-1">
- 	<div class="col-xs-6 col-md-offset-3">
- 		<div class="col-md-12">
- 			<h3>Personal Information</h3>
- 			<div class="form-group">
- 				<label class="control-label">Full Name</label>
- 				<input maxlength="100" type="text" required="required" class="form-control" placeholder="Full Name" />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">Identity Number</label>
- 				<input maxlength="13" type="text" required="required" class="form-control" placeholder="Identity Number"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">Date of Birth</label>
- 				<input  type="text" required="required" class="form-control" placeholder="Date of Birth"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">Email</label>
- 				<input   type="email" required="required" class="form-control" placeholder="Email Address"/>
- 			</div>
-
- 			<div class="form-group">
- 				<label class="control-label">Phone number</label>
- 				<input maxlength="10" type="text" required="required" class="form-control" placeholder="Phone number"  />
- 			</div>
- 			<label  id ="gender" class="control-label">Gender</label>
-
- 			<div class="form-group">
-
- 				<input name="group100" type="radio" id="radio100">
- 				<label for="radio100">Male</label>
- 			</div>
-
- 			<div class="form-group">
- 				<input name="group100" type="radio" id="radio101" checked>
- 				<label for="radio101">Female</label>
- 			</div>
- 			<button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
- 		</div>
- 	</div>
- </div>
- <div class="row setup-content" id="step-2">
- 	<div class="col-xs-6 col-md-offset-3">
- 		<div class="col-md-12">
- 			<h3>Residential Information</h3>
- 			<div class="form-group">
- 				<label class="control-label">Street Address</label>
- 				<input maxlength="100" type="text" required="required" class="form-control" placeholder="Street Address" />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">suburb</label>
- 				<input maxlength="13" type="text" required="required" class="form-control" placeholder="suburb"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">City/Town</label>
- 				<input  type="text" required="required" class="form-control" placeholder="City/Town"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">Zip Code</label>
- 				<input maxlength="10" type="text" required="required" class="form-control" placeholder="Zip Code"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">Municipality</label>
- 				<input maxlength="10" type="text" required="required" class="form-control" placeholder="Municipality"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">District</label>
- 				<input maxlength="10" type="text" required="required" class="form-control" placeholder="District"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">Province</label>
- 				<input maxlength="10" type="text" required="required" class="form-control" placeholder="Province"  />
- 			</div>
-
- 			<button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
- 		</div>
- 	</div>
- </div>
- <div class="row setup-content" id="step-3">
- 	<div class="col-xs-6 col-md-offset-3">
- 		<div class="col-md-12">
- 			<h3>Owner Information</h3>
- 			<div class="form-group">
- 				<label class="control-label">Title Deed</label>
- 				<input type="text" required="required" class="form-control" placeholder="Title Deed" />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">House Type</label>
- 				<input type="text" required="required" class="form-control" placeholder="House Type"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">Registration Number</label>
- 				<input   type="text" required="required" class="form-control" placeholder="Registration Number"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">Registration Date</label>
- 				<input  type="text" required="required" class="form-control" placeholder="Registration Date"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">Purchase Price</label>
- 				<input type="text" required="required" class="form-control" placeholder="Purchase Price"  />
- 			</div>
- 			<div class="form-group">
- 				<label class="control-label">Purchase Date</label>
- 				<input type="text" required="required" class="form-control" placeholder="Purchase Date"  />
- 			</div>
- 			<div class="fileupload fileupload-new" data-provides="fileupload">
- 				<span class="btn btn-primary btn-file"><span class="fileupload-new">upload file</span>
- 				<span class="fileupload-exists"></span>         <input type="file" /></span>
- 				<span class="fileupload-preview"></span>
- 				<a href="#" class="close fileupload-exists" data-dismiss="fileupload" style="float: none">×</a>
- 			</div>
- 		</div>
- 		<button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
- 	</div>
- </div>
- <div class="row setup-content" id="step-4">
- 	<div class="col-xs-6 col-md-offset-4">
- 		<div class="col-md-12">
- 			<h3> Step 4</h3>
- 			<button class="btn btn-success btn-lg pull-right" type="submit">Submit</button>
- 		</div>
- 	</div>
- </div>
+				<button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+			</div>
+		</div>
+	</div>
+	<div class="row setup-content" id="step-3">
+		<div class="col-xs-6 col-md-offset-3">
+			<div class="col-md-12">
+				<h3>Owner Information</h3>
+				<div class="form-group">
+					<label class="control-label">Title Deed</label>
+					<input type="text" required="required" class="form-control" placeholder="Title Deed" />
+				</div>
+				<div class="form-group">
+					<label class="control-label">House Type</label>
+					<input type="text" required="required" class="form-control" placeholder="House Type"  />
+				</div>
+				<div class="form-group">
+					<label class="control-label">Registration Number</label>
+					<input   type="text" required="required" class="form-control" placeholder="Registration Number"  />
+				</div>
+				<div class="form-group">
+					<label class="control-label">Registration Date</label>
+					<input  type="text" required="required" class="form-control" placeholder="Registration Date"  />
+				</div>
+				<div class="form-group">
+					<label class="control-label">Purchase Price</label>
+					<input type="text" required="required" class="form-control" placeholder="Purchase Price"  />
+				</div>
+				<div class="form-group">
+					<label class="control-label">Purchase Date</label>
+					<input type="text" required="required" class="form-control" placeholder="Purchase Date"  />
+				</div>
+				<div class="fileupload fileupload-new" data-provides="fileupload">
+					<span class="btn btn-primary btn-file"><span class="fileupload-new">upload file</span>
+					<span class="fileupload-exists">Change</span>         <input type="file" /></span>
+					<span class="fileupload-preview"></span>
+					<a href="#" class="close fileupload-exists" data-dismiss="fileupload" style="float: none">×</a>
+				</div>
+			</div>
+			<button class="btn btn-primary nextBtn btn-lg pull-right" type="button" >Next</button>
+		</div>
+	</div>
+	<div class="row setup-content" id="step-4">
+		<div class="col-xs-6 col-md-offset-4">
+			<div class="col-md-12">
+				<h3> Step 4</h3>
+				<button class="btn btn-success btn-lg pull-right" type="submit">Submit</button>
+			</div>
+		</div>
+	</div>
 </form>
 
- </div>
+
+</div>
+
+
+
+
+
 
 
 <script type="text/javascript">
@@ -212,5 +217,5 @@
 	});
 
 
- 
+
 </script>
