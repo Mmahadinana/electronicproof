3679b793e89cc433bd86986369a95f2db7cf3d2d
Sybil Dichaba <sybil.dichaba@grupoacin.local>
2018-01-25 12:25:08 +0000

    update

--- application/views/makePDF/makepdf.php
+++ application/views/makePDF/makepdf.php
@@ -35,8 +35,13 @@
 
 foreach ($user_addinfor as $key ) {
 }
-
-
+ foreach ($owner_addinfor as $valowner ) {
+                //storing the data of the owner where resident lives
+                  $owner_id=$valowner ->owner;
+                  $property_id=$valowner ->property;
+                  $owner_name=$valowner->name;
+                  $owner_housetype=$valowner->house_type;
+                }
 // create new PDF document
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
@@ -73,21 +78,69 @@ $htmlcontent = "<html>
 						";
 		
 $htmlcontent .= "<td width='165'>" . date('M-d-Y') .      "</td></tr>";
-
+$htmlcontent .= "<tr>
+	    		    <td width='700'>&nbsp;</td>
+	  			  </tr>";
+$htmlcontent .= "<tr>
+	    		    <td width='700'>&nbsp;</td>
+	  			  </tr>";
 
 $htmlcontent .=  "<tr>
         		    <td width='165'>Address</td>
-		  	    	<td width='335' colspan='2'>58972</td>
+		  	    	<td width='335' colspan='2'>" .$key->door_number. ' '.$key->street_name."</td>
       			  </tr>";
 
 $htmlcontent .=  "<tr>
 	    		    <td width='500' colspan='3'>&nbsp;</td>
+	    		    <td width='335' colspan='3'>" .$key->street_name."</td>
+	    		  
 	  			  </tr>";	
-
 $htmlcontent .=  "<tr>
+	    		    <td width='500' colspan='4'>&nbsp;</td>
+	    		    <td width='335' colspan='4'>" .$key->town."</td>
+	    		  
+	  			  </tr>";
+$htmlcontent .=  "<tr>
+	    		    <td width='500' colspan='5'>&nbsp;</td>
+	    		    <td width='335' colspan='5'>" .$key->zip_code."</td>
+	    		  
+	  			  </tr>";
+$htmlcontent .=  "<tr>
+	    		    <td width='500' colspan='6'>&nbsp;</td>
+	    		    <td width='335' colspan='6'>" .$key->town."</td>
+	    		  
+	  			  </tr>";
+$htmlcontent .=  "<tr>
+	    		    <td width='500' colspan='7'>&nbsp;</td>
+	    		    <td width='335' colspan='7'>" .$key->manucipality."</td>
+	    		  
+	  			  </tr>";
+$htmlcontent .=  "<tr>
+	    		    <td width='500' colspan='8'>&nbsp;</td>
+	    		    <td width='335' colspan='8'>" .$key->district."</td>
+	    		  
+	  			  </tr>";
+$htmlcontent .=  "<tr>
+	    		    <td width='500' colspan='9'>&nbsp;</td>
+	    		    <td width='335' colspan='9'>" .$key->province."</td>
+	    		  
+	  			  </tr>";	  			  	  			  
+	  			  
+$htmlcontent .= "<tr>
+	    		    <td width='700'>&nbsp;</td>
+	  			  </tr>";
+$htmlcontent .= "<tr>
+	    		    <td width='700'>&nbsp;</td>
+	  			  </tr>";
+$htmlcontent .= "<tr>
+	    		    <td width='700'>&nbsp;</td>
+	  			  </tr>";
 
-                    <td width='165'>Resident</td>
-                  </tr>";
+
+$htmlcontent .= "<tr>
+	    		    <td width='700'>&nbsp;</td>
+	  			  </tr>";	  			  	  			  	  			  	  			  
+
 $htmlcontent .=  "<tr>
 		  	    	<td width='335' colspan='4'></td>
 		  	    	<td width='165'><?php echo $key->door_number. ' '.$key->street_name?></td>
@@ -97,18 +150,26 @@ $htmlcontent .= "<tr>
 	  			  </tr>";	
 $htmlcontent .= "<tr>
 	    		    <td width='500'>&nbsp;</td>
-	  			  </tr>";		  			  
+	  			  </tr>";	
+$htmlcontent .= "<tr>
+	    		    <td width='700'>&nbsp;</td>
+	  			  </tr>";	  			  
 $htmlcontent .=  "<tr>
-					
-                    <td width='700'>This is to confirm that (Name,ID Number) stays at the above mentioned address since". ' ' .date('M-d-Y') ." until today.The house type owned by (Owner)   </td>
+					 
+                    <td style='width:70%'>This is to confirm that" .'  '."<strong>" . $key->name . "</strong>".' '. "ID number" .' ' ."<strong>". $key->identitynumber ."</strong>"." stays at the above mentioned address since". ' ' .date('M-d-Y') ." until today.The" . ' '."<strong>" . $owner_housetype . "</strong>" . ' ' ."is owned by" . ' '."<strong>" .$owner_name. "</strong>"."</td>
                   </tr>";
+
 $htmlcontent .=  "<tr>
 		  	    	
 		  	    	<td width='2000'>This letter will be valid for only three months,starting from the date issued.</td>
       			  </tr>"; 
 $htmlcontent .= "<tr>
 	    		    <td width='700'>&nbsp;</td>
-	  			  </tr>";	      			                   
+	  			  </tr>";
+$htmlcontent .= "<tr>
+	    		    <td width='700'>SIGNATURE___________________________________</td>
+	  			  </tr>";	  			  
+	  			      			                   
 $htmlcontent .= "</table></body></html>";
 
 // output the HTML content
